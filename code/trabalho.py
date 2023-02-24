109774c469973c5049c9837bede29dbc29380488
Felipe <felipednerling@gmail.com>
2023-02-24 15:54:47 -0300

    Versão inicial e definição de funções

---
+++ code/trabalho.py
@@ -0,0 +1,10 @@
+#Funcoes necessarias: Incluir, Consultar, Alterar, Excluir, Listar todos e Sair.
+#def incluir():
+    
+#def consultar():
+    
+#def alterar():
+    
+#def excluir():
+    
+#def listar():
