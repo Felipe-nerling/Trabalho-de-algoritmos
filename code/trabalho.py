5d656beda209039a3b96e98dd871c3d0084613cd
Felipe <felipednerling@gmail.com>
2023-03-01 23:26:56 -0300

    Definição da classe e criação da função incluir

--- code/trabalho.py
+++ code/trabalho.py
@@ -1,6 +1,36 @@
-#Funcoes necessarias: Incluir, Consultar, Alterar, Excluir, Listar todos e Sair.
-#def incluir():
-    
+class compromisso:
+    data = None
+    hora = None
+    duracao = None
+    descricao = None
+
+#Funções
+def incluir():
+    while 1:
+        x = compromisso()
+        x.data = input("Data: ")
+        x.hora = input("Hora: ")
+        if x.data in agenda and x.hora in agenda: #Verifica se existem outros compromissos na mesma data e horario
+            op = input("\nJá existem compromissos marcados para a data e horário informados. Salvar mesmo assim?\n(1)Sim\n(2)Não\n")
+            if op == 1:
+                continue
+            elif op == 2:
+                pass
+            else:
+                print("ERRO: Opção inválida.")
+                break
+        x.duracao = input("Duração: ")
+        x.descricao = input("Descrição: ")            
+        agenda.append(x)
+        opt = int(input("\nIncluir outro compromisso?\n(1)Sim\n(2)Não\n"))
+        if opt == 1:
+            continue
+        elif opt == 2:
+            break
+        else:
+            print("ERRO: Opção inválida.")
+            break
+
 #def consultar():
     
 #def alterar():
@@ -8,3 +38,5 @@
 #def excluir():
     
 #def listar():
+
+agenda = []#Vetor que armazena os compromissos
