ea6abefef625758df96634311adcc9dab31f70e5
Felipe <felipednerling@gmail.com>
2023-03-02 22:03:54 -0300

    Ajustes na função alterar e adicionada a função excluir.

--- code/trabalho.py
+++ code/trabalho.py
@@ -7,6 +7,7 @@ class compromisso:
 #Funções
 def incluir():
     while 1:
+        print("--INCLUIR COMPROMISSO--")
         c = compromisso()
         c.data = input("\nData: ")
         c.hora = input("Hora: ")
@@ -39,6 +40,7 @@ def incluir():
 
 def consultardata():
     while 1:
+        print("--CONSULTAR POR DATA--")
         dt = input("\nDigite a data a ser consultada:\n")
         cont = 0
         for i in range(len(agenda)):
@@ -59,6 +61,7 @@ def consultardata():
 
 def consultardh():
     while 1:
+        print("--CONSULTAR POR DATA E HORA--")
         dt = input("\nDigite a data a ser consultada:\n")
         hr = input("\nDigite a hora a ser consultada:\n")
         cont = 0
@@ -80,6 +83,7 @@ def consultardh():
 
 def alterar():
     while 1:
+        print("--ALTERAR COMPROMISSOS--")
         dt = input("\nDigite a data do compromisso:\n")
         hr = input("\nDigite a hora do compromisso:\n")
         ind = []
@@ -90,7 +94,10 @@ def alterar():
             else:
                 if i == (len(agenda))-1 and len(ind) == 0:
                     print("Compromisso não encontrado!")
-        if len(ind) > 1:
+        if len(ind) == 1:
+            agenda[ind[0]].descricao = input("\nDigite a nova descrição:\n")
+            agenda[ind[0]].duracao = input("\nDigite a nova duração:\n")
+        elif len(ind) > 1:
             x = input("\nExistem 2 ou mais compromissos agendados nesse horário.\nDigite a descrição do compromisso a ser alterado:\n")
             for i in range(len(ind)):
                 if agenda[ind[i]].descricao == x:
@@ -108,7 +115,39 @@ def alterar():
             print("\nERRO: Opção inválida.")
             break
 
-#def excluir():
+def excluir():
+    while 1:
+        print("--EXCLUIR COMPROMISSOS--")
+        dt = input("\nDigite a data do compromisso:\n")
+        hr = input("\nDigite a hora do compromisso:\n")
+        ind = []
+        for i in range(len(agenda)):
+            if agenda[i].data == dt and agenda[i].hora == hr:
+                print("Descrição: ", agenda[i].descricao, "\tHorário: ", agenda[i].hora, "\tDuração: ", agenda[i].duracao)
+                ind.append(i)
+            else:
+                if i == (len(agenda))-1 and len(ind) == 0:
+                    print("Compromisso não encontrado!")
+        if len(ind) == 1:
+            agenda.pop(ind[0])
+            print("\nCompromisso removido com sucesso!")
+        elif len(ind) > 1:
+            x = input("\nExistem 2 ou mais compromissos agendados nesse horário.\nDigite a descrição do compromisso a ser excluído:\n")
+            for i in range(len(ind)):
+                if agenda[ind[i]].descricao == x:
+                    agenda.pop(ind[i])
+                    print("\nCompromisso removido com sucesso!")
+                    break
+                elif i == (len(ind))-1:
+                    print("\nERRO: Descrição não encontrada")
+        opt = int(input("\nExcluir outro compromisso?\n(1)Sim\n(2)Não\n"))
+        if opt == 1:
+            continue
+        elif opt == 2:
+            break
+        else:
+            print("\nERRO: Opção inválida.")
+            break
     
 #def listar():
 
@@ -128,5 +167,7 @@ while 1:
             print("\nERRO: Opção inválida.")
     elif op == 3:
         alterar()
+    elif op == 4:
+        excluir()
     else:
         break
